4806f645076d5d276909a3e87b24ac3606c4ae71
megaofmegalodon <testtrolium@gmail.com>
2026-07-18 10:21:58 -0400

    feat: add packet opcode to packet data map

--- src/utils/core/PacketMap.ts
+++ src/utils/core/PacketMap.ts
@@ -60,4 +60,67 @@ const PacketMap = {
     }
 } as const;
 
+export interface MOOMOO_CLIENT_TO_SERVER_MAP {
+    "z": [id: number, isWeapon: boolean];
+    "K": [id: 1];
+    "e": [];
+    "6": [msg: string];
+    "M": [data: { name: string, moofoll: string, skin: number }];
+    "9": [angle: number | null];
+    "0": [];
+    "S": [];
+    "D": [angle: number];
+    "F": [autoGatherToggle: number, dir: number];
+    "H": [id: number];
+    "P": [sid: number, accepted: boolean];
+    "Q": [sid: number];
+    "b": [sid: string];
+    "N": [];
+    "L": [name: string];
+    "c": [buy: boolean, id: number, index: boolean];
+};
+
+export interface MOOMOO_SERVER_TO_CLIENT_MAP {
+    "io-init": [id: string, seed: number, salt: string, idk: number];
+    "A": [data: { teams: any[] }];
+    "C": [yourSID: number];
+    "D": [
+        data: any,
+        isYou: boolean
+    ];
+    "I": [data: number[]];
+    "E": [id: string];
+    "Q": [sid: number];
+    "R": [ownerSID: number];
+    "a": [data: (string | number)[]];
+    "O": [sid: number, health: number];
+    "9": [x: number, y: number];
+    "g": [data: any];
+    "1": [
+        /** sid of the alliance that should be removed */ sid: string
+    ];
+    "2": [sid: number, name: string];
+    "3": [team: string, isOwner: boolean];
+    "5": [type: boolean, id: number, index: number];
+    "4": [data: (number | string)[]];
+    "8": [x: number, y: number, value: number, type: number];
+    "S": [index: number, value: number];
+    "J": [sid: number];
+    "H": [data: number[]];
+    "G": [data: (number | string)[]];
+    "N": [index: "stone" | "wood" | "food" | "kills" | "points", value: number, updateView: boolean];
+    "T": [xp: number | undefined, mxp: number | undefined, age: number | undefined];
+    "V": [data: number[] | null, wpn: boolean];
+    "0": [];
+    "P": [];
+    "6": [sid: number, message: string];
+    "K": [sid: number, didHit: boolean, index: number];
+    "L": [dir: number, sid: number];
+    "M": [sid: number, dir: number];
+    "U": [points: number, age: number];
+    "X": [x: number, y: number, dir: number, range: number, speed: number, indx: number, layer: number, sid: number];
+    "Y": [sid: number, range: number];
+    "7": [data: number[]];
+}
+
 export default PacketMap;

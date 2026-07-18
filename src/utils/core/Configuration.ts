ea6104b00605ee033d2647a6021afa155777eeb8
megaofmegalodon <testtrolium@gmail.com>
2026-07-18 15:37:43 -0400

    docs: adjust Configuration comments

--- src/utils/core/Configuration.ts
+++ src/utils/core/Configuration.ts
@@ -3,7 +3,7 @@ const Configuration = {
     MAP_SIZE: 14400,
     SHAME_DURATION: 5_000, // 5 seconds, original game is 30 seconds
     SERVER_UPDATE_SPEED: 1e3 / 9, // don't change this if u don't want the game to explode
-    ANTI_CHEAT: false, // basically enables a decent anticheat
+    ANTI_CHEAT: false, // basically enables a somewhat decent anticheat
     AUTO_REGEN_COOLDOWN: 1000,
     PLAYER_DECELERATION: .993,
     MAX_SCREEN_WIDTH: 1920,

459380a22af79863f3499e719a22664341c8c80d
megaofmegalodon <testtrolium@gmail.com>
2026-07-18 11:25:44 -0400

    chore: add SERVER_UPDATE_SPEED to Configurations

--- src/utils/core/Configuration.ts
+++ src/utils/core/Configuration.ts
@@ -1,7 +1,8 @@
 const Configuration = {
     PLAYER_SPEED: .0016,
     MAP_SIZE: 14400,
-    SHAME_DURATION: 15_000 // 15 seconds
+    SHAME_DURATION: 15_000, // 15 seconds
+    SERVER_UPDATE_SPEED: 1e3 / 9 // don't change this if u don't want the game to explode
 } as const;
 
 export default Configuration;

170142cb270381e7973ca15970ad2e8b19e755e9
megaofmegalodon <testtrolium@gmail.com>
2026-08-01 14:32:14 -0400

    fix: leaderboard doesn't gather dead players

--- src/utils/core/getLeaderboardData.ts
+++ src/utils/core/getLeaderboardData.ts
@@ -2,6 +2,6 @@ import PlayerManager from "@core/PlayerManager";
 
 export default function getLeaderboardData() {
     const data: any[] = [];
-    PlayerManager.players.slice(0, 10).forEach(e => data.push(e.sid, e.name, 1e6));
+    PlayerManager.players.filter(e => e.isAlive).slice(0, 10).forEach(e => data.push(e.sid, e.name, 1e6));
     return data;
 }
